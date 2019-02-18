67c70d02687ef61fe09cee52de055d1b455f0fdd
theodor.vorzellner <theodor.vorzellner@AMS-GYM-EDV1-04.gym.ams.local>
2019-02-18 15:01:09 +0100

    Bubblesort Ausgabe mit Text

--- HalloWeltProjekt/src/basics/bubblesort.java
+++ HalloWeltProjekt/src/basics/bubblesort.java
@@ -5,13 +5,15 @@ public class bubblesort {
 	public static void main(String[] args) {
 
 		int[] zahlen = { 3, 6, 1, 9, 2, 5 };
-
+		System.out.println("Unsortierte Zahlen" + "\n");
 		for (int i = 0; i < zahlen.length; i++) {
-
+			
 			System.out.println(zahlen[i]);
 
 		}
 
+		System.out.println("\n" + "----------------");
+		
 		for (int j = zahlen.length; j > 1; j--) {
 			for (int i = 0; i < (j - 1); i++) {
 				if (zahlen[i] > zahlen[i + 1]) {
@@ -22,7 +24,7 @@ public class bubblesort {
 			}
 
 		}
-		System.out.println();
+		System.out.println("\n" + "Sortierte Zahlen" + "\n");
 		for (int i = 0; i < zahlen.length; i++) {
 			System.out.println(zahlen[i]);
 		}
